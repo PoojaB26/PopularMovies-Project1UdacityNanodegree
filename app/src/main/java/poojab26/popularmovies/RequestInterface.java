38f89d167e60a31128ecc3e68aeb5259498cd7e1
poojab26 <pbhaumik26@gmail.com>
2017-10-04 21:21:57 +0530

    Added API utilities

---
+++ app/src/main/java/poojab26/popularmovies/RequestInterface.java
@@ -0,0 +1,24 @@
+package poojab26.popularmovies;
+
+import poojab26.popularmovies.Model.PopularMoviesList;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+/**
+ * Created by pblead26 on 04-Oct-17.
+ */
+
+public interface RequestInterface {
+/*
+
+    @Headers("user_key: c031832bac8def9a5efadcb27718dfeb")
+    @GET("geocode")
+    Call<RestaurantList> getRestaurantList(@Query("lat")String lat, @Query("lon")String lon);
+*/
+
+
+    @GET("movie/popular")
+    Call<PopularMoviesList> getPopularMovies(@Query("api_key") String apiKey);
+
+}
---
+++ app/src/main/java/poojab26/popularmovies/Utilities/APIClient.java
@@ -0,0 +1,34 @@
+package poojab26.popularmovies.Utilities;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by pblead26 on 04-Oct-17.
+ */
+
+public class APIClient {
+
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getClient() {
+
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.themoviedb.org/3/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build();
+
+
+        return retrofit;
+    }
+
+
+}
