68747dc490b38dbae75395b35e80ed32dd108b0f
poojab26 <pbhaumik26@gmail.com>
2017-10-06 01:45:01 +0530

    UI for details screen

--- app/src/main/java/poojab26/popularmovies/Activity/DetailsActivity.java
+++ app/src/main/java/poojab26/popularmovies/Activity/DetailsActivity.java
@@ -4,14 +4,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
-import poojab26.popularmovies.Model.PopularMoviesList;
-import poojab26.popularmovies.Model.Movie;
-import poojab26.popularmovies.R;
 import poojab26.popularmovies.ApiInterface;
+import poojab26.popularmovies.Model.Movie;
+import poojab26.popularmovies.Model.PopularMoviesList;
+import poojab26.popularmovies.R;
 import poojab26.popularmovies.Utilities.APIClient;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -19,12 +22,15 @@ import retrofit2.Response;
 
 public class DetailsActivity extends AppCompatActivity {
     TextView tvMovieTitle, tvSynopsis, tvRating, tvRelease;
+    ImageView tvMovieBackground;
     ApiInterface apiInterface;
+    String BASE_PATH = "http://image.tmdb.org/t/p/w342/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+        tvMovieBackground = (ImageView)findViewById(R.id.imgBackground);
         tvMovieTitle = (TextView)findViewById(R.id.tvOrigTitle);
         tvSynopsis = (TextView)findViewById(R.id.tvSynopsis);
         tvRating = (TextView)findViewById(R.id.tvRating);
@@ -45,11 +51,13 @@ public class DetailsActivity extends AppCompatActivity {
             public void onResponse(Call<PopularMoviesList> call, Response<PopularMoviesList> response) {
 
                 List<Movie> movies = response.body().getMovies();
+                String path = movies.get(pos).getBackdropPath();
+                Picasso.with(getApplicationContext()).load(BASE_PATH+path).into(tvMovieBackground);
 
                 tvMovieTitle.setText(movies.get(pos).getOriginalTitle());
                 tvSynopsis.setText(movies.get(pos).getOverview());
                 tvRating.setText(String.valueOf(movies.get(pos).getVoteAverage()));
-                tvRelease.setText(movies.get(pos).getReleaseDate());
+                tvRelease.setText("Release Date : " +movies.get(pos).getReleaseDate());
 
 
             }
